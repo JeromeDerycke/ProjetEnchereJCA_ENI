dc0392dd47531a76d6dca5bb215aeb89f34cd654
JeromeDerycke <jeromederycke@gmail.com>
2023-02-14 12:51:22 +0100

    First commit

---
+++ ProjetEnchereJCA/src/main/java/fr/eni/enchere/bo/Enchere.java
@@ -0,0 +1,49 @@
+package fr.eni.enchere.bo;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Enchere implements Serializable {
+
+	private static final long serialVersionUID = 1L; 
+	
+
+
+	private LocalDate dateEnchere;
+	private int montantEnchere;
+	
+	
+	public Enchere () {
+		
+	}
+	
+	
+	
+	public Enchere(LocalDate dateEnchere, int montantEnchere) {
+		super();
+		this.dateEnchere = dateEnchere;
+		this.montantEnchere = montantEnchere;
+	}
+
+	public LocalDate getDateEnchere() {
+		return dateEnchere;
+	}
+
+	public void setDateEnchere(LocalDate dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+
+	public int getMontantEnchere() {
+		return montantEnchere;
+	}
+
+	public void setMontantEnchere(int montantEnchere) {
+		this.montantEnchere = montantEnchere;
+	}
+
+	
+	
+	
+	
+	
+}
