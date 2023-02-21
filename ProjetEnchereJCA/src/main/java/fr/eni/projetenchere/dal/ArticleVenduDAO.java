32a8b3aed9c8ec4e1166913ba9a0e7cbc2bedd5a
JeromeDerycke <jeromederycke@gmail.com>
2023-02-21 16:26:53 +0100

    suite

--- ProjetEnchereJCA/src/main/java/fr/eni/projetenchere/dal/ArticleVenduDAO.java
+++ ProjetEnchereJCA/src/main/java/fr/eni/projetenchere/dal/ArticleVenduDAO.java
@@ -1,15 +1,22 @@
 package fr.eni.projetenchere.dal;
 
 import fr.eni.projetenchere.bo.ArticleVendu;
+import fr.eni.projetenchere.bo.Utilisateur;
 
 public interface ArticleVenduDAO {
 
 	
 	public void insert (ArticleVendu article);
+	
 	public void delete (ArticleVendu article);
+	
 	public void select (ArticleVendu article);
+	
 	public void selectAll (ArticleVendu article);
+	
 	public void update (ArticleVendu article);
+	
+	public ArticleVendu select(String nomArticle);
 	
 	
 }
